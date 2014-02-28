0c1690d61729767db05ea783419e99d7d5f62dc2
Chris French <chris@ammonitenetworks.com>
2014-02-28 13:51:29 -0700

    Stripe base

---
+++ src/Stripe/Admin/Routes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dsc\Stripe;
+
+/**
+ * Group class is used to keep track of a group of routes with similar aspects (the same controller, the same f3-app and etc)
+ */
+class Routes extends \Dsc\Routes\Group{
+	
+	
+	function __construct(){
+		parent::__construct();
+	}
+	
+	/**
+	 * Initializes all routes for this group
+	 * NOTE: This method should be overriden by every group
+	 */
+	public function initialize(){
+		$this->setDefaults(
+				array(
+					'namespace' => '\Dsc\Strip\Admin\Controllers',
+					'url_prefix' => '/stripe'
+				)
+		);
+		
+		$this->add( '', 'GET', array(
+								'controller' => 'Dashboard',
+								'action' => 'index'
+								));
+	}
+}
